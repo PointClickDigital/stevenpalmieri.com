1849752ffe28d4d4f36541d5410e73c00b92b7be
PointClick <pointclick.digital@gmail.com>
2021-05-08 09:04:25 +0000

    clean up

--- wp-content/themes/stevenpalmieri/functions.php
+++ wp-content/themes/stevenpalmieri/functions.php
@@ -32,38 +32,6 @@ include('login-editor.php');
 
 
 
-/*
- * Part 1. Add Link (Tab) to My Account menu
- */
-add_filter ( 'woocommerce_account_menu_items', 'pcd_upgrade_link', 40 );
-function pcd_upgrade_link( $menu_links ){
- 
-	$menu_links = array_slice( $menu_links, 0, 5, true ) 
-	+ array( 'upgrade' => 'Upgrade' )
-	+ array_slice( $menu_links, 5, NULL, true );
- 
-	return $menu_links;
- 
-}
-/*
- * Part 2. Register Permalink Endpoint
- */
-add_action( 'init', 'pcd_add_endpoint' );
-function pcd_add_endpoint() {
- 
-	// WP_Rewrite is my Achilles' heel, so please do not ask me for detailed explanation
-	add_rewrite_endpoint( 'upgrade', EP_PAGES );
- 
-}
-/*
- * Part 3. Content for the new page in My Account, woocommerce_account_{ENDPOINT NAME}_endpoint
- */
-add_action( 'woocommerce_account_log-history_endpoint', 'pcd_my_account_endpoint_content' );
-function pcd_my_account_endpoint_content() {
- 
-}
-
-
 /** Require Terms and Conditions At Woocommerce Login */
  
 
